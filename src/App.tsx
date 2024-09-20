838668624637cd0b5a30abd67e5302a8ee74418f
Lukas <lukas802480@gmail.com>
2024-09-20 18:07:34 +0200

    change createBrowseRoutern on HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+
+// import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import FilmsPage from './pages/FilmsPage'
 import WelcomePage from './pages/WelcomePage'
 
-const router = createBrowserRouter([
-  {path: '/', element: <WelcomePage />},
-  {path: '/films', element: <FilmsPage />},
-])
+// const router = createBrowserRouter([
+//   {path: '/', element: <WelcomePage />},
+//   {path: '/films', element: <FilmsPage />},
+// ])
+
+// function App() {
+// 	return <RouterProvider router={router} />
+// }
 
 function App() {
-	return <RouterProvider router={router} />
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<WelcomePage />} />
+				<Route path='/about' element={<FilmsPage />} />
+			</Routes>
+		</Router>
+	)
 }
 
 export default App
