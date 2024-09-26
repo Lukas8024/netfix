8933423da45966f38ba2a233bc3dcdb77fe57821
Lukas <lukas802480@gmail.com>
2024-09-26 20:47:18 +0200

    add carousel

--- src/components/TopFive.tsx
+++ src/components/TopFive.tsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
 
 import { DUMMY_FILMS } from '../dummy-video'
-import Film from './Film'
+
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+
 
 const TopFive = () => {
-	const [selectedFilms, setSelectedFilms] = useState<
-		{ id: number; image: string; title: string; description: string }[]
-	>([])
+	const [selectedFilms, setSelectedFilms] =
+		useState<{ id: number; image: string; title: string; description: string }[]>(DUMMY_FILMS)
 
 	// Funkcja losująca pięć róznych obiektów
 	const getRandomObjects = () => {
@@ -17,20 +19,95 @@ const TopFive = () => {
 	useEffect(() => {
 		// Ustawiamy losowe obiekty przy pierwszym renderze
 		const randomObjects = getRandomObjects()
+		// console.log(randomObjects)
 		setSelectedFilms(randomObjects)
 	}, [])
 
 	return (
-		<div id='films'>
-			<h2>Top 5</h2>
-			<ul className='films'>
-				{selectedFilms.map(film => (
-					<li key={film.id}>
-                        <Film {... film}/>
-					</li>
-				))}
-			</ul>
-		</div>
+		<>
+			<div id='topfilms'>
+				<h2>Top 5</h2>
+			</div>
+
+			{/* <ul className='topfilms'>
+						{selectedFilms.map(film => (
+							<li key={film.id}>
+								<img src={film.image} alt={film.title} />
+							</li>
+						))}
+					</ul> */}
+
+			<Carousel
+				additionalTransfrom={0}
+				arrows
+				autoPlay
+				autoPlaySpeed={2000}
+				centerMode={false}
+				className="topfilms"
+				containerClass="container-with-dots"
+				dotListClass=""
+				draggable
+				focusOnSelect={false}
+				infinite
+				itemClass="items"
+				keyBoardControl
+				minimumTouchDrag={80}
+				pauseOnHover
+				renderArrowsWhenDisabled={false}
+				renderButtonGroupOutside={false}
+				renderDotsOutside={false}
+				responsive={{
+				  desktop: {
+					breakpoint: {
+					  max: 3000,
+					  min: 1024
+					},
+					items: 3,
+					partialVisibilityGutter: 40
+				  },
+				  mobile: {
+					breakpoint: {
+					  max: 464,
+					  min: 0
+					},
+					items: 1,
+					partialVisibilityGutter: 30
+				  },
+				  tablet: {
+					breakpoint: {
+					  max: 1024,
+					  min: 464
+					},
+					items: 2,
+					partialVisibilityGutter: 30
+				  }
+				}}
+				rewind={false}
+				rewindWithAnimation={false}
+				rtl={false}
+				shouldResetAutoplay
+				showDots={false}
+				sliderClass=""
+				slidesToSlide={2}
+				swipeable
+			>
+				<div>
+					<img src={selectedFilms[0].image} />
+				</div>
+				<div>
+					<img src={selectedFilms[1].image} />
+				</div>
+				<div>
+					<img src={selectedFilms[2].image} />
+				</div>
+				<div>
+					<img src={selectedFilms[3].image} />
+				</div>
+				<div>
+					<img src={selectedFilms[4].image} />
+				</div>
+			</Carousel>
+		</>
 	)
 }
 
