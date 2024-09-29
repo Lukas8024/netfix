675e5f0d36c9217d718e7e1452502a151f4b36d4
Łukasz Bojdak <121494252+Lukas8024@users.noreply.github.com>
2024-09-29 20:08:13 +0200

    add accordion

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useState } from 'react'
+
+import AccordionItem from './AccordionItem'
+import AccordionTitle from './AccordionTitle'
+import AccordionContenet from './AccordionContent'
+
+interface AccordionContextType {
+	toggleItem: any
+	openItemId: any
+}
+
+const AccordionContext = createContext<AccordionContextType | undefined>(undefined)
+
+export function useAccordionContext() {
+	const ctx = useContext(AccordionContext)
+
+console.log(ctx);
+
+	if (!ctx) {
+		throw new Error('Accordion related component must be wrraped by <Accordion>')
+	}
+
+	return ctx
+}
+
+export default function Accordion({ children, className }: any) {
+	const [openItemId, setOpenItemId] = useState()
+
+	function toggleItem(id: any) {
+		setOpenItemId(prevId => (prevId === id ? null : id))
+	}
+
+	const contextValue = {
+		toggleItem,
+		openItemId,
+	}
+
+	return (
+		<AccordionContext.Provider value={contextValue}>
+			<ul className={className}>{children}</ul>
+		</AccordionContext.Provider>
+	)
+}
+
+Accordion.Item = AccordionItem
+Accordion.Title= AccordionTitle
+Accordion.Content = AccordionContenet
---
+++ src/components/Accordion/AccordionItem.tsx
@@ -0,0 +1,26 @@
+import { createContext, useContext } from 'react'
+
+interface AccordionItemContextType{
+ id: any;
+}
+
+const AccordionItemContext = createContext<AccordionItemContextType | undefined>(undefined)
+
+export function useAccordionItemContext() {
+	const ctx = useContext(AccordionItemContext)
+
+	console.log(ctx);
+	if (!ctx) {
+		throw new Error('AccordionItem - related components must be wrrap by <Accordion.Item>')
+	}
+
+	return ctx
+}
+
+export default function AccordionItem({ id, children, className }: any) {
+	return (
+		<AccordionItemContext.Provider value={id}>
+			<li className={className}>{children}</li>
+		</AccordionItemContext.Provider>
+	)
+}
